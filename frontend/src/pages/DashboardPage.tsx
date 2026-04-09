b1cd4f449dd19700806f3458db007f4481640d32
wylieglover <rg11445@georgiasouthern.edu>
2026-04-08 22:04:30 -0400

    Trying fix 13

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -16,7 +16,7 @@ export default function DashboardPage() {
   }
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+    <div style={{ position: "fixed", inset: 0 }}>
       {/* Logout Overlay */}
       <div 
         style={{ 
